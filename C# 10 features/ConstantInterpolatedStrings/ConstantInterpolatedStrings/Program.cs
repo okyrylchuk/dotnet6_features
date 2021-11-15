8e88c345749a3d4f635c4a6208396719314834e2
Simon McKenna <sshnug.si+github@gmail.com>
2021-11-15 20:33:23 +1030

    Fix output and enable implicit usings

--- C# 10 features/ConstantInterpolatedStrings/ConstantInterpolatedStrings/Program.cs
+++ C# 10 features/ConstantInterpolatedStrings/ConstantInterpolatedStrings/Program.cs
@@ -1,19 +1,12 @@
-﻿    
+﻿
 // https://twitter.com/okyrylchuk/status/1436049784104800259
 
 const string name = "Oleg";
 const string greeting = $"Hello, {name}.";
 
-const string yolo = "YOLO";
-
-[Obsolete($"This method is obsolote because of {yolo}")]
+[Obsolete($"This method is obsolete because of {greeting}")]
 static void Test()
 { }
 
 Test();
-Console.WriteLine(yolo); // Output: Hello, Oleg.
-
-
-
-
-
+Console.WriteLine(greeting); // Output: Hello, Oleg.
