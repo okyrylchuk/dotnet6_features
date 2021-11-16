ba2c0711511b5716e341bc2e0b4f23f81c84adda
Simon McKenna <sshnug.si+github@gmail.com>
2021-11-16 18:29:42 +1030

    Remove method and attribute

--- C# 10 features/ConstantInterpolatedStrings/ConstantInterpolatedStrings/Program.cs
+++ C# 10 features/ConstantInterpolatedStrings/ConstantInterpolatedStrings/Program.cs
@@ -1,12 +1,6 @@
-﻿
-// https://twitter.com/okyrylchuk/status/1436049784104800259
+﻿// https://twitter.com/okyrylchuk/status/1436049784104800259
 
 const string name = "Oleg";
 const string greeting = $"Hello, {name}.";
 
-[Obsolete($"This method is obsolete because of {greeting}")]
-static void Test()
-{ }
-
-Test();
 Console.WriteLine(greeting); // Output: Hello, Oleg.
