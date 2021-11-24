f099430d7af07a938bb29eafd917c01110ac8910
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-11-24 22:55:40 +0100

    Add 'Translation for IsNullOrWhitespace' sample

---
+++ EF Core 6/Translation for IsNullOrWhitespace/Translation for IsNullOrWhitespace/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Entities
+                    .Where(e => string.IsNullOrWhiteSpace(e.Property))
+                    .ToQueryString();
+
+Console.WriteLine(query);
+// Output:
+// SELECT [e].[Id], [e].[Property]
+// FROM [Entities] AS[e]
+// WHERE [e].[Property] IS NULL OR ([e].[Property] = N'')
+
+class Entity
+{
+    public int Id { get; set; }
+    public string Property { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Entity> Entities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6IsNullOrWhiteSpace");
+}
