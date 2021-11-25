c39550a9add92bca1ce58b9765ec2c02c427532d
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-11-25 22:22:35 +0100

    Add 'Translate Substring with 1 param' sample

---
+++ EF Core 6/Translate Substring with single parameter/Translate Substring with single parameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+context.People.Add(new Person { Name = "John" });
+context.People.Add(new Person { Name = "Bred" });
+context.People.Add(new Person { Name = "Ron" });
+await context.SaveChangesAsync();
+
+var result = await context.People
+    .Select(a => new { Name = a.Name.Substring(1) })
+    .ToListAsync();
+// Translated SQL: 
+// SELECT SUBSTRING([p].[Name], 1 + 1, LEN([p].[Name])) AS [Name]
+// FROM [People] AS [p]
+
+result.ForEach(p => Console.WriteLine(p.Name));
+// Output:
+// ohn
+// red
+// on
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6Substring");
+}
+
