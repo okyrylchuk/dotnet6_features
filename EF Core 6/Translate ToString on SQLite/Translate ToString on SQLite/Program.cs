6b7c12b002b142c2aed07dbc2d18604fa9f80454
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-11-27 22:32:52 +0100

    Add 'Translate ToString on SQLite' sample

---
+++ EF Core 6/Translate ToString on SQLite/Translate ToString on SQLite/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.People
+    .Where(u => EF.Functions.Like(u.PhoneNumber.ToString(), "%368%"))
+    .ToQueryString();
+
+Console.WriteLine(query);
+// Query:
+// SELECT "p"."Id", "p"."Name", "p"."PhoneNumber"
+// FROM "People" AS "p"
+// WHERE CAST("p"."PhoneNumber" AS TEXT) LIKE '%368%'
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public long PhoneNumber { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=:memory:");
+}
