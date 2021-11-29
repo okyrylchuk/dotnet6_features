748d75e677674ec8eccbdf7d007f846e6e8389b4
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-11-29 22:30:20 +0100

    Add 'Remove last ORDER By clause' sample

---
+++ EF Core 6/Remove last ORDER BY clause/Remove last ORDER BY clause/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Blogs
+    .Include(b => b.Posts.Where(p => p.Rating > 3))
+    .ToQueryString();
+
+Console.WriteLine(query);
+
+class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Rating { get; set; }
+    public Blog Blog { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6RemoveLastOrderByClause");
+}
+
