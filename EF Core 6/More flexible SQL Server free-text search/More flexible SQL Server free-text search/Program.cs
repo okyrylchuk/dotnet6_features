366cc66cb43d2841d8574255a4505686b3cd6215
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-01 23:01:42 +0100

    Add 'FreeText on binary columns' sample

---
+++ EF Core 6/More flexible SQL Server free-text search/More flexible SQL Server free-text search/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Posts
+    .Where(p => EF.Functions.FreeText(EF.Property<string>(p, "Content"), "Searching text"))
+    .ToQueryString();
+
+Console.WriteLine(query);
+// SELECT[p].[Id], [p].[Content], [p].[Title]
+// FROM[Posts] AS[p]
+// WHERE FREETEXT([p].[Content], N'Searching text')
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public byte[] Content { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Post>()
+            .Property(x => x.Content)
+            .HasColumnType("varbinary(max)");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6FlexibleTextSearch");
+}
+
