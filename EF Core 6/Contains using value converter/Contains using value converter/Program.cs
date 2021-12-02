4dff2066eff1d1bece746e4036972b62b7dbc615
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-02 22:34:45 +0100

    Add 'Contains with value converter' sample

---
+++ EF Core 6/Contains using value converter/Contains using value converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+using var context = new ExampleContext();
+
+var query = context.People
+    .Where(e => EF.Functions.Contains(e.FullName, "Oleg"))
+    .ToQueryString();
+
+Console.WriteLine(query);
+// SELECT[p].[Id], [p].[FullName]
+// FROM[People] AS[p]
+// WHERE CONTAINS([p].[FullName], N'Oleg')
+
+class Person
+{
+    public int Id { get; set; }
+    public FullName FullName { get; set; }
+}
+
+public class FullName
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>()
+            .Property(x => x.FullName)
+            .HasConversion(
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                    v => JsonSerializer.Deserialize<FullName>(v, (JsonSerializerOptions)null));
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6Contains");
+}
+
