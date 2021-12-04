98d448f3ea90387235905a8b463404eff97439a5
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-04 22:31:01 +0100

    Add 'Encrypted SQLite db' sample

---
+++ EF Core 6/Encrypted SQLite/Encrypted SQLite/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=EncryptedDb.db;Mode=ReadWriteCreate;Password=password");
+}
+
