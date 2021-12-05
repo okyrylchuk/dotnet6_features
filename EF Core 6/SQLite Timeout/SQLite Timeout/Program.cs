c85c53b0b6afa3fa7b1d2eb3307207804df45680
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-05 22:15:42 +0100

    Add 'SQLite command timeout in CS' sample

---
+++ EF Core 6/SQLite Timeout/SQLite Timeout/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    // 60 seconds as the default timeout for commands created by connection
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=Test.db;Command Timeout=60");
+}
+
