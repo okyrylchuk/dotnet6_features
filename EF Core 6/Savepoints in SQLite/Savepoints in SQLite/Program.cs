0fa2563e1b3a33a479995b6d49543052385ad4e6
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-06 22:48:24 +0100

    Add 'Savepoints in SQLite' sample

---
+++ EF Core 6/Savepoints in SQLite/Savepoints in SQLite/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+
+var dbPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\Savepoints.db"));
+
+using var connection = new SqliteConnection($"Data Source={dbPath}");
+connection.Open();
+using var transaction = connection.BeginTransaction();
+
+// The insert is committed to the database
+using (var command = connection.CreateCommand())
+{
+    command.CommandText = @"INSERT INTO People (Name) VALUES ('Oleg')";
+    command.ExecuteNonQuery();
+}
+
+transaction.Save("MySavepoint");
+
+// The update is not commited since savepoint is rolled back before commiting the transaction
+using (var command = connection.CreateCommand())
+{
+    command.CommandText = @"UPDATE People SET Name = 'Not Oleg' WHERE Id = 1";
+    command.ExecuteNonQuery();
+}
+
+transaction.Rollback("MySavepoint");
+transaction.Commit();
+
