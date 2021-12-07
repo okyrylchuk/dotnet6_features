0d8e18ad519ffd69bb608e9a47327089fb54f740
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-07 23:00:12 +0100

    Add 'Cosmos supports FromSqlRaw' sample

---
+++ EF Core 6/Cosmos FromSql/Cosmos FromSql/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Posts
+    .FromSqlRaw("SELECT * FROM Posts")
+    .ToQueryString();
+
+Console.WriteLine(query);
+// SELECT c
+// FROM (
+//    SELECT * FROM Posts
+// ) c
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseCosmos(
+        "https://localhost:8081",
+        "[key]",
+        databaseName: "ExampleDB");
+}
+
