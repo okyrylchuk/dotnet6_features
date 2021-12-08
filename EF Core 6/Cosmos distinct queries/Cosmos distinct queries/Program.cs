de551204611e8f62c1a12815dc53a0d12692de84
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-08 23:01:56 +0100

    Add 'Cosmos distinct' sample

---
+++ EF Core 6/Cosmos distinct queries/Cosmos distinct queries/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Posts
+    .Select(p => p.Rating)
+    .OrderBy(r => r)
+    .Distinct()
+    .ToQueryString();
+
+Console.WriteLine(query);
+// SELECT DISTINCT c["Rating"]
+// FROM root c
+// WHERE (c["Discriminator"] = "Post")
+// ORDER BY c["Rating"]
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Rating { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseCosmos(
+        "https://localhost:8081",
+        "[key]",
+        databaseName: "ExampleDB");
+}
+
