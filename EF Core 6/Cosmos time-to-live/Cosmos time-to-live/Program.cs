9b8839a79f4b74bab5e9a2477f0e40a18ad23309
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-09 23:02:51 +0100

    Add 'Cosmos time-to-live' sample

---
+++ EF Core 6/Cosmos time-to-live/Cosmos time-to-live/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Rating { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Post>(entityTypeBuilder =>
+            {
+                entityTypeBuilder.HasDefaultTimeToLive(100);
+                entityTypeBuilder.HasAnalyticalStoreTimeToLive(200);
+            });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseCosmos(
+        "https://localhost:8081",
+        "[key]",
+        databaseName: "ExampleDB");
+}
