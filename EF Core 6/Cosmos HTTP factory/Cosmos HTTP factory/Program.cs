48857eb479ab7397cdd4df3d296216006da616bf
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-11 22:26:56 +0100

    Add 'Cosmos HTTP client factory' sample

---
+++ EF Core 6/Cosmos HTTP factory/Cosmos HTTP factory/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+class ExampleContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseCosmos(
+        "https://localhost:8081",
+        "[key]",
+        databaseName: "ExampleDB",
+        cosmosOptionsBuilder =>
+        {
+            cosmosOptionsBuilder.HttpClientFactory(
+                () => new HttpClient(
+                    new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback =
+                            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                    }));
+        });
+}
