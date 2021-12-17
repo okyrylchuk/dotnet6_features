939a143f15012d1e7d030df6b2567b4661142041
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-17 21:51:15 +0100

    Add 'Cosmos Math translations' sample

---
+++ EF Core 6/Cosmos Math translations/Cosmos Math translations/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+var query = context.Products
+    .Where(p => Math.Abs(p.Price) == 100
+    || Math.Round(p.Price) > 100)
+    .ToQueryString();
+
+Console.WriteLine(query);
+// SELECT c
+// FROM root c
+// WHERE ((c["Discriminator"] = "Product")
+// AND((ABS(c["Price"]) = 100.0)
+// OR(ROUND(c["Price"]) > 100.0)))
+
+class Product
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseCosmos(
+        "https://localhost:8081",
+        "[key]",
+        databaseName: "MathTranlationsDB");
+}
+
