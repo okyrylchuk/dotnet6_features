afbb89f6572487a7041ee5a98d8665d7aad23871
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-19 20:57:03 +0100

    Add 'Split queries for non naviation collections' sample

---
+++ EF Core 6/Split-queries for non-navigation collections/Split-queries for non-navigation collections/Migrations/20211219193131_Initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Split_queries_for_non_navigation_collections.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ EF Core 6/Split-queries for non-navigation collections/Split-queries for non-navigation collections/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new ExampleContext();
+
+#region Populate DB
+// var blog = new Blog { Name = ".NET Blog"};
+// blog.Posts.Add(new Post { Title = "First .NET post" });
+// blog.Posts.Add(new Post { Title = "Second Java post" });
+// blog.Posts.Add(new Post { Title = "Third .NET post" });
+
+// context.Blogs.Add(blog);
+
+// await context.SaveChangesAsync();
+#endregion
+
+var blogsWithDotnetPosts = await context.Blogs
+    .Select(b => new
+    {
+        b,
+        Posts = b.Posts.Where(p => p.Title.Contains(".NET")),
+    })
+    .AsSplitQuery()
+    .ToListAsync();
+
+class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Blog Blog { get; set; }
+}
+
+class ExampleContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options
+        .EnableSensitiveDataLogging()
+        .LogTo(Console.WriteLine)
+        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore6SplitQueries");
+}
