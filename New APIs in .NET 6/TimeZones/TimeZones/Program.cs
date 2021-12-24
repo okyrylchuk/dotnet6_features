9a7f26ca50a0ddf0dd9cfa000f1968a5843b30cd
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-24 16:36:11 +0100

    Add 'Time zones' sample

---
+++ New APIs in .NET 6/TimeZones/TimeZones/Program.cs
@@ -0,0 +1,14 @@
+﻿var plIanaTimeZoneId = "Europe/Warsaw";
+
+var plTimeZone = TimeZoneInfo.FindSystemTimeZoneById(plIanaTimeZoneId);
+Console.WriteLine(plTimeZone.DisplayName);
+// (UTC+01:00) Sarajevo, Skopje, Warsaw, Zagreb
+
+TimeZoneInfo.TryConvertIanaIdToWindowsId(plIanaTimeZoneId, out var plWindowsId);
+Console.WriteLine(plWindowsId);
+// Central European Standard Time
+
+TimeZoneInfo.TryConvertWindowsIdToIanaId(plWindowsId, out plIanaTimeZoneId);
+Console.WriteLine(plIanaTimeZoneId);
+// Europe/Warsaw
+
