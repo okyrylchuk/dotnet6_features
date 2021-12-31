65dee014bae01dff1967fce91dc1234587abbde3
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-12-31 15:36:22 +0100

    Add 'Caller expression attribute' sample

---
+++ C# 10 features/Caller expression attribute/Caller expression attribute/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+void TestMethod(object obj, [CallerArgumentExpression("obj")] string? message = null)
+{
+    Console.WriteLine("Expression is " + message);
+}
+
+TestMethod(new object());
+TestMethod("hello");
+TestMethod(1 + 2 + 3);
+TestMethod(() => { });
+
+// Output: 
+// Expression is new object()
+// Expression is "hello"
+// Expression is 1 + 2 + 3
+// Expression is () => { }
+
