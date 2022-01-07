c9f9d72e18845831b05bef1b848a35dc997efbde
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-01-07 19:56:02 +0100

    Add 'Improved definite assignment' sample

---
+++ C# 10 features/Improved definite assignment/Improved definite assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿var example = new Example();
+if ((example != null && example.TryGetValue(out SomeValue value1)) == true)
+{
+    var _ = value1.Id;
+}
+
+// With null conditional operator
+if (example?.TryGetValue(out SomeValue value2) == true)
+{
+    var _ = value2.Id;
+}
+
+// With null-coalescing operator
+if (example?.TryGetValue(out SomeValue value3) ?? false)
+{
+    var _ = value3.Id;
+}
+
+public class SomeValue
+{
+    public int Id { get; set; }
+}
+
+public class Example
+{
+    public bool TryGetValue(out SomeValue value)
+    {
+        value = new SomeValue();
+        return true;
+    }
+}
+
