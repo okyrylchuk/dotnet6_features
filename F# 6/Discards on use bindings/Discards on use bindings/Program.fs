22f893074ac18bd3c2b07fadd75b7d3209f32c44
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-01-14 21:34:43 +0100

    Add 'Discards on use bindings' sample

---
+++ F# 6/Discards on use bindings/Discards on use bindings/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+let disposable () = { new IDisposable with
+                            member x.Dispose() = ()}
+
+let example () =
+    use _ = disposable ()
+    printfn "discard on use binding"
