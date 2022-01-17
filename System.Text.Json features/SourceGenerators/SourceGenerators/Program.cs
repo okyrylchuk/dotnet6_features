b24dbf9ccb1797756373bdd2beaea051e9d755ae
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-01-17 23:35:05 +0100

    Add 'Source generator' sample

---
+++ System.Text.Json features/SourceGenerators/SourceGenerators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var example = new Example { Message = "Hello, world!" };
+var json = JsonSerializer.Serialize(example, ExampleContext.Default.Example);
+Console.WriteLine(json);
+// Output:
+// {
+//    "Message": "Hello, world!"
+// }
+
+var deserializedExample = JsonSerializer.Deserialize<Example>(json, ExampleContext.Default.Example);
+Console.WriteLine(deserializedExample.Message);
+//// Output:
+//// Hello, world!
+
+internal class Example
+{
+    public string Message { get; set; }
+}
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(Example))]
+internal partial class ExampleContext : JsonSerializerContext
+{
+}
