01545586cb6f3963ea464c1a5dc962e5d69a0cc9
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-01-27 23:27:03 +0100

    Add 'Additional collection functions' sample

---
+++ F# 6/Additional collection functions/Additional collection functions/Program.fs
@@ -0,0 +1,19 @@
+﻿
+
+let list = [ 1; 2; 3 ]
+
+printfn "%A" (List.insertAt 2 4 list)
+// [1; 2; 4; 3]
+
+printfn "%A" (List.removeAt 0 list)
+// [2; 3]
+
+printfn "%A" (List.updateAt 0 5 list)
+// [5; 2; 3]
+
+printfn "%A" (List.insertManyAt 0 [9; 8] list)
+// [9; 8; 1; 2; 3]
+
+printfn "%A" (List.removeManyAt 1 2 list)
+// [1]
+
