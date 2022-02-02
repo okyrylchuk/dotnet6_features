1c577c9ce82e2b65d9b5ab28e80c7745390db69d
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-02-02 21:48:49 +0100

    Add 'Http/3 preview' samples

---
+++ Preview features/Http3/Http3Server/Http3Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Listen(IPAddress.Any, 5001, listenOptions =>
+    {
+            // Use HTTP/3
+            listenOptions.Protocols = HttpProtocols.Http3;
+        listenOptions.UseHttps();
+    });
+});
+
+var app = builder.Build();
+app.Run();
+
---
+++ Preview features/Http3/Use HTTP3 with HttpClient/Use HTTP3 with HttpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Net;
+
+var client = new HttpClient()
+{
+    DefaultRequestVersion = HttpVersion.Version30,
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
+};
+
+var response = await client.GetAsync("https://localhost:5001/");
+Console.WriteLine($"version: {response.Version}");
+
