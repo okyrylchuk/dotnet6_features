9a76332f558d513519a901b837cdf0bdbc97a0a4
okyrylchuk <kyrylchuk.oleg@gmail.com>
2022-02-09 20:52:37 +0100

    Add 'As pattern' sample

---
+++ F# 6/AsPattern/AsPattern/Program.fs
@@ -0,0 +1,16 @@
+﻿type Point = Point of int * int
+
+let processInput (input: obj) =
+    match input with
+    | :? Point as Point (x, y) -> printfn $"A Point: {x}, {y}"
+    | :? (int * int) as (x, y) -> printfn $"A tuple: {x}, {y}"
+    | _ -> printfn "Bad input"
+
+processInput(Point(1, 2))
+processInput((1, 2))
+processInput([1,2])
+// Output:
+// A Point: 1, 2
+// A tuple: 1, 2
+// Bad input
+
